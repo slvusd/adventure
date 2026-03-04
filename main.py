40311a45f13bb7a1fb70a7e067249e9f3a3ab86d
Eric Brown <ebrown@slvusd.org>
2026-03-04 14:00:13 -0500

    first pass

---
+++ main.py
@@ -0,0 +1,22 @@
+from rooms import goon, basement, space, armory, mgr, dungeon, juvie, office, island
+
+def main():
+    current_room = "juvie"
+    
+    rooms = {
+        "goon": goon.run,
+        "basement": basement.run,
+        "space": space.run,
+        "armory": armory.run,
+        "mgr": mgr.run,
+        "dungeon": dungeon.run,
+        "juvie": juvie.run,
+        "office": office.run,
+        "island": island.run,
+    }
+
+    while current_room in rooms:
+        current_room = rooms[current_room]()
+
+main()
+
---
+++ rooms/juvie.py
@@ -0,0 +1,10 @@
+# rooms/juvie.py
+
+def run():
+    print("\n === Juvie Hall ===")
+
+    while True:
+        choice = input("Where do you go? > ").strip().lower()
+
+        return choice
+
