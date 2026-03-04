7b4b7a025672062c0888c085f2fa63a9a81c5add
Stefan Gogosanu <29sgogosanu@slvusd.org>
2026-03-04 14:47:47 -0500

    add my room

---
+++ rooms/mgr.py
@@ -0,0 +1,10 @@
+# rooms/mgr.py
+
+def run():
+	print("This is mason's goon room")
+	print("MASON GERSHENSON ONLY")
+
+	while True:
+		choice = input("Get to another room").strip().lower()
+
+	return choice
