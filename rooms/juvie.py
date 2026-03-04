8e28bac283841ffc83cef14ee39ea4fbfe13565e
Eric Brown <ebrown@slvusd.org>
2026-03-04 14:57:10 -0500

    update juvie

--- rooms/juvie.py
+++ rooms/juvie.py
@@ -6,6 +6,9 @@ def run():
 
     while True:
         choice = input("Where do you go? > ").strip().lower()
+        if choice != "sky":
+          print("You are trapped in Juvie hall!!!")
+          continue
 
         return choice
 
