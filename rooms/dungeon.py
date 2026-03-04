eea685193b4ef137321637462fda5925888e0099
Razvan Gogosanu <181163337+razvan136@users.noreply.github.com>
2026-03-04 11:32:15 -0800

    Create dungeon.py

---
+++ rooms/dungeon.py
@@ -0,0 +1,10 @@
+# rooms/juvie.py
+
+def run():
+    print("\n === evil dungeon ===")
+    print("get ready for a fun time")
+
+    while True:
+        choice = input("Where do you want to go?? > ").strip().lower()
+
+            return choice
