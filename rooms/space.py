c037bd45094b5a0b80eff6ea58c1d071a3a7cfda
Mason G <Crate-1127>
2026-03-04 15:00:47 -0500

    start space.py

---
+++ rooms/space.py
@@ -0,0 +1,19 @@
+#Space.py
+import random
+import math
+
+parts = ["Engine", "Navigation", "Fuel Tank", "Fuel", "Oxidizer","Turret"]
+
+class planet:
+    def __init__(self, place, part_needed):
+        self.place = place
+        self.part_needed = part_needed
+    
+    def part_get(self):
+            
+def run():
+    print("==SPACE==")
+    print("zoom zoom you're in space!")
+
+    
+    
