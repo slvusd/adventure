bb918a44fab056309b345a78c7f387cd9a952cab
Sky <29ssmith@slvusd.org>
2026-03-09 14:13:28 -0400

    fixed the bug

--- rooms/sky.py
+++ rooms/sky.py
@@ -2,7 +2,7 @@ def run():
 	print("=== sky ===")
 	print("you are not allowed to fly in the sky")
 
-	while True
+	while True:
 		choice = input("did you fly in the sky or the air ")
 		if  choice != "sky":
 			print("you go to jail")
