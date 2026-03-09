8de9907b5ee0a07ef8dccb1baf6ba0dfb7c49aff
Sky <29ssmith@slvusd.org>
2026-03-09 14:29:00 -0400

    Added feature

--- rooms/sky.py
+++ rooms/sky.py
@@ -1,10 +1,11 @@
 def run():
 	print("=== sky ===")
-	print("you are not allowed to fly in the sky")
+	print("you are not allowed to fly in the sky, did you? y/n")
 
 	while True:
-		choice = input("did you fly in the sky or the air ")
-		if  choice != "sky":
-			print("you go to jail")
-
-		return choice
+		choice = input("you are not allowed to fly in the sky, did you? y/n ")
+		if  choice != "y":
+			print("you go to juvie")
+			return  "juvie"
+		else:
+			return "dungeon"y
