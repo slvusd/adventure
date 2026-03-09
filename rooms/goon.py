d55eb693415ca5966edb156af32da24a72f7c17b
noah <29nwheelock@slvusd.org>
2026-03-09 14:47:25 -0400

    first try

---
+++ rooms/goon.py
@@ -0,0 +1,18 @@
+#rooms/goon.py
+
+def run():
+	print("\n == goon room ==")
+	print("you are not able to come in")
+
+
+	while True:
+		choice = input("did you sneak in or are you not here: ")
+		if choice !="yes" :
+			print("get out")
+
+
+		return "sky"
+
+
+
+
