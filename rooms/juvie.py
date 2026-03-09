6c41f6709da6a29052ad188dd08f2e83b3770b52
Eric Brown <ebrown@slvusd.org>
2026-03-09 14:20:00 -0400

    support inventory

--- rooms/juvie.py
+++ rooms/juvie.py
@@ -1,14 +1,22 @@
 # rooms/juvie.py
 
-def run():
+def run(inventory):
     print("\n === Juvie Hall ===")
+    print(f"You currently have: {inventory}")
     print("You were very bad!")
+
+    inventory += ["ball"]
+
+    if "knife" in inventory:
+        pass
+    else:
+        inventory += ["knife"]
 
     while True:
         choice = input("Where do you go? > ").strip().lower()
-        if choice != "sky":
+        if choice != "mgr":
           print("You are trapped in Juvie hall!!!")
-          continue
+          print("But I'll let you go!")
 
         return choice
 
