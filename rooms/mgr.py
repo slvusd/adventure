63cc89d3fd33733e259cb555c442854c1b71de8c
attentkebab <29sgogosanu@slvusd.org>
2026-03-16 11:25:12 -0700

    Update mgr.py

--- rooms/mgr.py
+++ rooms/mgr.py
@@ -1,4 +1,5 @@
 # rooms/mgr.py
+# MGR code © 2026 by Stefan Gogosanu is licensed under CC BY-NC-SA 4.0. To view a copy of this license, visit https://creativecommons.org/licenses/by-nc-sa/4.0/
 
 def run():
 	print("This is mason's goon room")
